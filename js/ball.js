da9e80e763960c3f2d48389f3bea883a5f22a470
Martin Thomas <smbthomas@mac.com>
2023-05-01 20:38:26 +0100

    Added bricks function to draw 5 rows of coloured bricks.

--- js/ball.js
+++ js/ball.js
@@ -7,27 +7,46 @@ let xSpeed = 9;
 let ySpeed = 6;
 let down = true;
 let right = true;
-let dia = 20;
+let dia = 26;
 let rad = dia / 2;
+let width = 800;
+let height = 800;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(width, height);
 }
 
 function draw() {
   background(0);
+  fill(0, 255, 0);
+  // rect(100, 100, 50, 20);
+  bricks(60, 0, 0, 255);
+  bricks(80, 0, 255, 255);
+  bricks(100, 0, 255, 0);
+  bricks(120, 255, 255, 0);
+  bricks(140, 255, 0, 0);
+  fill(200, 200, 200);
   circle(0 + x, 0 + y, dia, dia);
   if (right) 
     x += xSpeed;
   else
     x -= xSpeed;
-  if (x > (800 - rad)) right = false;
+  if (x > (width - rad)) right = false;
   if (x < (0 + rad)) right = true;
-  
-    if (down) 
+  if (down) 
     y += ySpeed;
   else
     y -= ySpeed;
-  if (y > (800 - rad)) down = false;
+  if (y > (height - rad)) down = false;
   if (y < (0 + rad)) down = true;
+}
+
+function bricks(ypos, rcol, gcol , bcol) {
+  let xpos = 0;
+  noStroke();
+  fill(rcol, gcol, bcol);
+  for (i = 0; i < 16; i++) {
+    rect(xpos, ypos, 50, 20);
+    xpos += 50;
+  }
 }
