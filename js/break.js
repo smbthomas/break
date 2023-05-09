9f2133f71a894fa31ce4d85bdec08844ace2808f
Martin Thomas <smbthomas@mac.com>
2023-05-09 23:29:47 +0100

    Added bat class to this file.

--- js/break.js
+++ js/break.js
@@ -3,6 +3,7 @@
  Copyright (c) Stephen Martin Beynon Thomas, May 2023
 
  */
+
 const width = 800;
 const height = 800;
 const col = ["red", "yellow", "royalblue", "magenta", "lawngreen", "cyan"];
@@ -15,7 +16,8 @@ for(let i = 0; i < numWalls; i++) {
   wall[i] = new Wall(i, col[i]);
 }
 
-let ball = new Ball(100, 300, 26, true, true, 7, 17);
+let ball = new Ball(100, 300, 26, true, true, 3, 4);
+let bat = new Bat(350);
 
 function setup() {
   let canvas = createCanvas(width, height);
@@ -28,5 +30,6 @@ function draw() {
     wall[i].display();
   }
   ball.display();
+  bat.display();
   ball.move();
 }
