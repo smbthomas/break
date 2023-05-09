4c4f8ddde69fdb08b2b4c6612a27cf6f0b8d5cfe
Martin Thomas <smbthomas@mac.com>
2023-05-09 14:38:09 +0100

    Added logic to remove bricks in wall.

--- js/ball.js
+++ js/ball.js
@@ -22,18 +22,42 @@ class Ball {
       this.y += this.ys;
     else
       this.y -= this.ys;
-    if (this.y > (height - rad)) this.down = false;
-    if (this.y < (200 + rad)) {
+    if (this.y > (height - rad)) {
+      this.down = false;
+    }
+    let xpos = Math.floor(this.x / 50);
+    let ypos = 0;
+    switch(ceiling[xpos]) {
+    case 200:
+      ypos = 0;
+    break;
+    case 180:
+      ypos = 1;
+    break;
+    case 160:
+      ypos = 2;
+    break;
+    case 140:
+      ypos = 3;
+    break;
+    case 120:
+      ypos = 4;
+    break;
+    case 100:
+      ypos = 5;
+    break;
+    default:
+      ypos = 6;
+    }
+    if (this.y < (ceiling[xpos] + rad)) {
       this.down = true;
+      wall[ypos].brick[xpos].exists = false;
+      ceiling[xpos] -= 20;
     }
   }
   
-  getPos() {
-    return Math.floor(this.x / 50);
-  }
-  
   display() {
-    fill(200, 200, 200);
+    fill(220);
     strokeWeight(0);
     circle(this.x, this.y, this.d);
   }
