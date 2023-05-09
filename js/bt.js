d475d07a70d4389de0752f892dbacfeb4523b2d2
Martin Thomas <smbthomas@mac.com>
2023-05-09 14:39:01 +0100

    Various changes re bricks and wall.

--- js/bt.js
+++ js/bt.js
@@ -1,22 +1,29 @@
+/*
+ Breakout game using JavaScript classes
+ Copyright (c) Stephen Martin Beynon Thomas, May 2023
+
+ */
 const width = 800;
 const height = 800;
 const col = ["red", "yellow", "royalblue", "magenta", "lawngreen", "cyan"];
 const numWalls = 6;
+var floor = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var ceiling = [200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200];
 
 let wall = [];
 for(let i = 0; i < numWalls; i++) {
   wall[i] = new Wall(i, col[i]);
 }
 
-let ball = new Ball(100, 100, 26, true, true, 7, 5);
+let ball = new Ball(100, 300, 26, true, true, 7, 17);
 
 function setup() {
-  createCanvas(width, height);
+  let canvas = createCanvas(width, height);
+  canvas.parent('grid-content');
 }
 
 function draw() {
   background(0);
-
   for(let i = 0; i < numWalls; i++) {
     wall[i].display();
   }
